12ac44767714d3cd3a99bb131d6edad6fceb2299
Khrystyna Kindrat <102920051+ChristabelKindrat@users.noreply.github.com>
2023-04-10 12:40:24 +0300

    Update Main.java
    Delete comments

--- src/lesson5/Main.java
+++ src/lesson5/Main.java
@@ -8,25 +8,25 @@ public class Main {
 
         //Join method of thread:
 
-        Result result = new Result(); //Create new obj
+        Result result = new Result();
 
-        int num_first = GenerateRandomNumber.generateNumber(); //Generate random number
+        int num_first = GenerateRandomNumber.generateNumber();
         CustomThread firstThead = new CustomThread();
         firstThead.takeEven(num_first, result);
         firstThead.start();
 
         try {
-            firstThead.join(); //Wait when active thead will be finished
+            firstThead.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
-        int num_second = GenerateRandomNumber.generateNumber(); //Generate random number
+        int num_second = GenerateRandomNumber.generateNumber();
         CustomThread secondThead = new CustomThread();
         secondThead.takeEven(num_second, result);
         secondThead.start();
 
-        System.out.println(result.getNumber()); //Show result number
+        System.out.println(result.getNumber());
 
         //Wait & Notify methods:
 
@@ -34,19 +34,19 @@ public class Main {
         final int finishedNumber = 100;
         List<Integer> primes = new ArrayList<>();
 
-        CustomThread thirdThead = new CustomThread(); //Create and start new Thread
+        CustomThread thirdThead = new CustomThread();
         thirdThead.start();
 
         synchronized (thirdThead) {
-            thirdThead.wait(); //Wait when numbers filtered and adds/not add to arrayList
+            thirdThead.wait();
         }
         thirdThead.numbersBetween(startNumber, finishedNumber, primes);
         
          synchronized (thirdThead) {
-            thirdThead.notify(); ////Restore thead and finish a program
+            thirdThead.notify();
         }
 
-        System.out.println(primes); //Show all prime numbers
+        System.out.println(primes);
         System.out.println("Prime numbers between " + startNumber + " and " + finishedNumber);
     }
 }
